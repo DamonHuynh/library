a74b0dcba19c8d452b22ee649f1bf719b8f8f304
Damon Huynh <dhuynh10072007@gmail.com>
2024-01-06 02:58:56 -0800

    Set up constructor; Add addBookToLibrary()
    addBookToLibrary adds the book to the myLibrary[], which stores all of
    the user's inputted books

---
+++ script.js
@@ -0,0 +1,13 @@
+const myLibrary = [];
+
+function Book(name, author, imgUrl, pages, hasRead) {
+    this.name = name;
+    this.author = author;
+    this.imgUrl = imgUrl;
+    this.pages = pages;
+    this.hasRead = hasRead;
+}
+
+function addBookToLibrary(name, author, imgUrl, pages, hasRead) {
+    myLibrary.push(new Book(name, author, imgUrl, pages, hasRead));
+}
