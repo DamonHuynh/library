fce51c79e90b90751422b3fb252eeb2bdb848ef9
Damon Huynh <dhuynh10072007@gmail.com>
2024-01-06 20:59:17 -0800

    Remove IMGURL option;Submit now stores input value

--- script.js
+++ script.js
@@ -4,21 +4,28 @@ const addBtn = document.querySelector(".add");
 const cancelBtn = document.querySelector(".cancel");
 const confirmBtn = document.querySelector(".confirm");
 const form = document.querySelector("form");
+const nameForm = document.querySelector("#name");
+const authorForm = document.querySelector("#author");
+const pagesForm = document.querySelector("#pages");
+const hasReadForm = document.querySelector("#hasRead");
 
 
 
-function Book(name, author, imgUrl, pages, hasRead) {
+function Book(name, author, pages, hasRead) {
     this.name = name;
     this.author = author;
-    this.imgUrl = imgUrl;
+
     this.pages = pages;
     this.hasRead = hasRead;
 }
 
-function addBookToLibrary(name, author, imgUrl, pages, hasRead) {
-    myLibrary.push(new Book(name, author, imgUrl, pages, hasRead));
+function addBookToLibrary(name, author, pages, hasRead) {
+    myLibrary.push(new Book(name, author, pages, hasRead));
 }
 
+function displayBook(){
+    
+}
 
 addBtn.addEventListener("click", () => {
     form.reset();
@@ -31,5 +38,16 @@ cancelBtn.addEventListener("click", () => {
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
+    let name = nameForm.value;
+    let author = authorForm.value;
+    let pages = pagesForm.value;
+    let hasRead = hasReadForm.value;
+    addBookToLibrary(name, author, pages, hasRead);
+    console.log(myLibrary);
     dialog.close();
 });
+
+confirmBtn.addEventListener("click", () => {
+
+} );
+
